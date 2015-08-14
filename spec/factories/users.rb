35bc3a7ea68ac83bb2b11bbf112c8760d0370bb9
oga <oga.ivc.s27@gmail.com>
2015-08-14 17:22:03 +0900

    add test for a validations of user model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,22 @@
 FactoryGirl.define do
   factory :user do
-    
+    # デフォルトテストユーザ
+    username    "seemore"
+    email       "seemore@seemore.inc"
+    nickname    "seemore"
+    birth_date  Date.new(2015, 8, 5)
+    sex         1
+    grade       "学部4年"
+    comment     "I'm Seemore. Seemore everything. I'm Seemore. Seemore everythig, no more ashmed."
+    # デフォルトテストユーザ2
+    factory :user2 do
+      username    "John"
+      email       "John@seemore.inc"
+      nickname    "John"
+      birth_date  Date.new(1990, 8, 5)
+      sex         0
+      grade       "学部4年"
+      comment     "John!"
+    end
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザ作成は" do
+    let(:user) { build(:user) }
+    let(:user2) { build(:user2) }
+
+    context "正しいパラメータの際" do
+      it "成功すること" do
+        expect(user.save).to eq(true)
+      end
+    end
+
+    context "usernameが空の場合" do
+      before { user.username = "" }
+      it "失敗すること" do
+        expect(user.save).to eq(false)
+      end
+    end
+
+    context "emailが空の場合" do
+      before { user.email = "" }
+      it "失敗すること" do
+        expect(user.save).to eq(false)
+      end
+    end
+
+    context "sexが空の場合" do
+      before { user.sex = nil }
+      it "失敗すること" do
+        expect(user.save).to eq(false)
+      end
+    end
+
+    context "usernameが重複するユーザが既に存在する場合" do
+      before do
+        user2.username = user.username
+        user.save
+      end
+      it "失敗すること" do
+        expect(user2.save).to eq(false)
+      end
+    end
+
+    context "emailが重複するユーザが既に存在する場合" do
+      before do
+        user2.email = user.email
+        user.save
+      end
+      it "失敗すること" do
+        expect(user2.save).to eq(false)
+      end
+    end
+
+    context "sexの値が[0,1,2]の場合" do
+      [0, 1, 2].each do |value|
+        before { user.sex = value }
+        it "成功すること" do
+          expect(user.save).to eq(true)
+        end
+      end
+    end
+
+    context "sexの値が[0,1,2]以外の場合" do
+      [-1, 3, 4].each do |value|
+        before { user.sex = value }
+        it "失敗すること" do
+          expect(user.save).to eq(false)
+        end
+      end
+    end
+  end
 end
