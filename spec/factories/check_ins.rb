94b49ee41f9220afc4e271be873cd048dab75b6c
oga <oga.ivc.s27@gmail.com>
2015-08-15 19:28:09 +0900

    add test for model 'CheckIn'.

--- spec/factories/check_ins.rb
+++ spec/factories/check_ins.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :check_in do
-    
+    user_id         1
+    restaurant_id   1
+    menu_id         1
+    comment         "おいしい"
   end
-
 end
--- spec/models/check_in_spec.rb
+++ spec/models/check_in_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe CheckIn, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:check_in) { build(:check_in) }
+
+  describe "チェックインの作成は" do
+    context "正常値の場合" do
+      it "成功すること" do
+        expect(check_in.save).to eq(true)
+      end
+    end
+    context "user_idがnilの場合" do
+      before { check_in.user_id = nil }
+      it "失敗すること" do
+        expect(check_in.save).to eq(false)
+      end
+    end
+    context "restaurant_idがnilの場合" do
+      before { check_in.restaurant_id = nil }
+      it "失敗すること" do
+        expect(check_in.save).to eq(false)
+      end
+    end
+    context "menu_idがnilの場合でも" do
+      before { check_in.menu_id = nil }
+      it "成功すること" do
+        expect(check_in.save).to eq(true)
+      end
+    end
+    context "commentが空でも" do
+      before { check_in.comment = "" }
+      it "成功すること" do
+        expect(check_in.save).to eq(true)
+      end
+    end
+  end
 end
